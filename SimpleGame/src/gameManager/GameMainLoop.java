102139c79d8f22c7397de7d0e0c0ba613986bcf5
Sebbyraft <alessandroseba1995@gmail.com>
2019-03-15 14:57:07 +0100

    display and loop logic

---
+++ SimpleGame/src/gameManager/GameMainLoop.java
@@ -0,0 +1,113 @@
+package gameManager;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.io.IOException;
+import display.GameWindow;
+
+public class GameMainLoop extends Canvas implements Runnable{
+
+	private static final long serialVersionUID = -2786430757538994538L;
+
+	private boolean running = true;
+	private Thread thread;
+	public int deltaMultiplier = 1;
+	public float systemTime = 0;
+
+	
+	public static void main(String[] args) throws IOException{
+		new GameMainLoop();
+	}
+	
+	public GameMainLoop() {
+		new GameWindow(this);
+		
+	}
+
+	private void render() {
+
+		BufferStrategy bs = this.getBufferStrategy();
+		
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		
+		Graphics g = bs.getDrawGraphics();
+		Graphics2D g2d = (Graphics2D)g;
+		
+		//*************************************************************************************************
+		g2d.setColor(new Color(22, 22, 22));
+		g2d.fillRect(0, 0, GameWindow.WIDTH, GameWindow.HEIGHT);
+		//*************************************************************************************************
+		
+	
+		//*************************************************************************************************
+
+		g.dispose();
+		bs.show();
+	}
+
+	@SuppressWarnings("unused")
+	public void run() {
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000/amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		
+		while(running) {
+			long now = System.nanoTime();
+			delta = delta + (now-lastTime)/ns;
+			lastTime = now;
+			
+			if (running) {
+				render();
+				systemTime=(float)delta*deltaMultiplier;
+			} else {
+				cleanUp();
+			}
+			
+			frames++;
+			
+			if(System.currentTimeMillis()-timer>1000) {
+				timer += 1000;
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		} catch(Exception e) {
+			System.err.println("Could not join thread!!");
+			e.printStackTrace();
+		}
+	}
+	
+	public void cleanUp() {
+	}
+	
+	public float getSystemTime() {
+		return systemTime;
+	}
+	
+	public void setDeltaMultiplier(int multiplier) {
+		this.deltaMultiplier = multiplier;
+	}
+
+}
